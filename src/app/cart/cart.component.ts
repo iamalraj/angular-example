1dd9cb75aaf61880bc0abe8a27fc6e91108652ed
iamalraj <ar4amal.raj@gmail.com>
2022-03-03 12:13:48 +0530

    App created with routing and froms : first commit

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,32 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit {
+  items = this.cartService.getItems();
+  checkoutForm = this.fromBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  constructor(
+    private cartService: CartService,
+    private fromBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    //process checkout data
+    this.items = this.cartService.clearCart();
+    window.alert('Your order has been submitted');
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+
+  ngOnInit(): void {}
+}
